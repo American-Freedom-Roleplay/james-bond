f7313e3ff1db2052df4db199b9813070bce8e517
AFRP <99163266+AFRP100807@users.noreply.github.com>
2022-04-18 15:16:29 -0700

    Add files via upload

---
+++ discord_perms/config.lua
@@ -0,0 +1,9 @@
+Config = {
+	DiscordToken = "NjEzODAzMDM2MjAwMDA5NzI5.XXwgXA.C0fjlGg-JXPELSINy8f871ZReRM",
+	GuildId = "597913431642865664",
+
+	-- Format: ["Role Nickname"] = "Role ID" You can get role id by doing \@RoleName
+	Roles = {
+		["WhiteList"] = "617231311270051864" -- This would be checked by doing exports.discord_perms:IsRolePresent(user, "TestRole")
+	}
+}
---
+++ discord_perms/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'gta5' }
+server_scripts {
+	"config.lua",
+	"server.lua"
+}
+
+server_export "IsRolePresent"
+server_export "GetRoles"
