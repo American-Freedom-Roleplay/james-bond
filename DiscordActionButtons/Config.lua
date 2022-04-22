53a52f50ac9b7b7312bce9400f9387150d0ec118
AFRP <99163266+AFRP100807@users.noreply.github.com>
2022-04-22 00:07:34 -0400

    Delete DiscordActionButtons directory

--- DiscordActionButtons/Config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-Config.Buttons = {
-    {index = 0,name = '%Website%',url = 'https://www.google.com'},
-    {index = 1,name = '%Connect%',url = 'fivem://connect/MyIpAddress:30120'}
-}
--- DiscordActionButtons/README.md
+++
@@ -1,3 +0,0 @@
-# DiscordActionButtons
-# Join Our Discord! https://discord.gg/BsbuFB8Bss
-# Our Script Store : https://cyberwebshop.tebex.io/
